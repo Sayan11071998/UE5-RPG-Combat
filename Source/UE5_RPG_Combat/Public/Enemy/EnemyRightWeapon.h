0d2e9e268f13cfc1e7ee4017948c3168a4d3073c
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-01-02 23:25:32 +0530

    Enemy Anim Notify State

---
+++ Source/UE5_RPG_Combat/Public/Enemy/EnemyRightWeapon.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "EnemyRightWeapon.generated.h"
+
+class AEnemy;
+
+UCLASS()
+class UE5_RPG_COMBAT_API UEnemyRightWeapon : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+public:
+	// ~ Begin UAnimNotifyState interface
+	virtual void NotifyBegin(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float TotalDuration,
+		const FAnimNotifyEventReference& EventReference	
+	) override;
+	
+	virtual void NotifyEnd(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference	
+	) override;
+	// ~ End UAnimNotifyState interface
+	
+	UPROPERTY()
+	TObjectPtr<AEnemy> Enemy;
+};
